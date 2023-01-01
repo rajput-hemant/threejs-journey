760c2187fa265eda5df87e832e5c2f828076eacd
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2023-01-02 01:30:45 +0530

    used gsap library to create tween animation
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- src/06-animations/src/main.ts
+++ src/06-animations/src/main.ts
@@ -1,5 +1,7 @@
-import "./style.css";
 import * as THREE from "three";
+import gsap from "gsap";
+
+import "./style.css";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl")!;
@@ -36,19 +38,22 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 
 // clock object to keep track of time
-const clock = new THREE.Clock();
+// const clock = new THREE.Clock();
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
 
 const tick = () => {
-	const elapsedTime = clock.getElapsedTime();
-	console.log(elapsedTime);
+	// 	const elapsedTime = clock.getElapsedTime();
+	// 	console.log(elapsedTime);
 
-	// update objects
-	// mesh.rotation.x = elapsedTime * Math.PI;
-	mesh.rotation.y = elapsedTime * Math.PI;
-	// mesh.rotation.z = elapsedTime * Math.PI;
+	// 	// update objects
+	// 	// mesh.rotation.x = elapsedTime * Math.PI;
+	// 	mesh.rotation.y = elapsedTime * Math.PI;
+	// 	// mesh.rotation.z = elapsedTime * Math.PI;
 
-	mesh.position.x = Math.sin(elapsedTime);
-	mesh.position.y = Math.cos(elapsedTime);
+	// 	mesh.position.x = Math.sin(elapsedTime);
+	// 	mesh.position.y = Math.cos(elapsedTime);
 
 	// render
 	renderer.render(scene, camera);
