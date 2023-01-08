511645d1c28ca18df74ece24cf475583f14fcd1c
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2023-01-09 00:06:25 +0530

    init
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ web/pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+
+const Homepage = () => {
+	return (
+		<>
+			<Head>
+				<title>Three.js Journey</title>
+				<meta name="description" content="" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/favicons/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicons/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicons/favicon-16x16.png"
+				/>
+			</Head>
+		</>
+	);
+};
+
+export default Homepage;
