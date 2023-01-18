bb9a4900c3207c20ecd321068f4df3920091bb14
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2023-01-18 14:45:42 +0530

    enabled vercel analytics
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,15 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+
+import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	);
 };
 
 export default App;
