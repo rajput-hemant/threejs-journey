0580d6d74b9a9a15162c0773ec26f8b9915b2694
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2023-01-29 23:34:08 +0530

    created Basic scene w/ OrbitControls using R3F
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- src/07-react-three-fiber/42-first-r3f-application/src/App.tsx
+++ src/07-react-three-fiber/42-first-r3f-application/src/App.tsx
@@ -1,5 +1,56 @@
+import { useRef } from "react";
+import { Group, Mesh } from "three";
+import { useFrame, extend, useThree, Object3DNode } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";;
+
+extend({ OrbitControls });
+
+declare module "@react-three/fiber" {
+	interface ThreeElements {
+		orbitControls: Object3DNode<OrbitControls, typeof OrbitControls>;
+	}
+}
+
 function App() {
-	return <></>;
+	const { camera, gl } = useThree();
+
+	const cubeRef = useRef<Mesh>(null!);
+	const groupRef = useRef<Group>(null!);
+
+	useFrame((_, delta) => {
+		cubeRef.current.rotation.y += delta;
+		// groupRef.current.rotation.y += delta;
+	});
+
+	return (
+		<>
+			<orbitControls args={[camera, gl.domElement]} />
+
+			<directionalLight position={[1, 2, 3]} />
+			<ambientLight intensity={0.5} />
+
+			<group ref={groupRef}>
+				<mesh position-x={-2}>
+					<sphereGeometry />
+					<meshStandardMaterial color="orange" />
+				</mesh>
+				<mesh
+					ref={cubeRef}
+					scale={1.5}
+					rotation-y={Math.PI * 0.25}
+					position-x={2}
+				>
+					{/* <sphereGeometry args={[1.5, 32, 32]} /> */}
+					<boxGeometry />
+					<meshStandardMaterial color="mediumpurple" />
+				</mesh>
+			</group>
+			<mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+				<planeGeometry />
+				<meshStandardMaterial color="greenyellow" />
+			</mesh>
+		</>
+	);
 }
 
 export default App;
--- src/07-react-three-fiber/42-first-r3f-application/src/main.tsx
+++ src/07-react-three-fiber/42-first-r3f-application/src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { Canvas } from "@react-three/fiber";
 
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<Canvas>
+			<App />
+		</Canvas>
 	</React.StrictMode>
 );
