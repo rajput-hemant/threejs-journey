5cb7a66a1ff042fe23d60e8fe728c7ace9e0d77e
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2023-01-31 23:52:25 +0530

    implemented ContactShadows, and added Sky
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- src/07-react-three-fiber/45-environment-and-staging/src/App.tsx
+++ src/07-react-three-fiber/45-environment-and-staging/src/App.tsx
@@ -2,19 +2,23 @@ import { useRef } from "react";
 import { Perf } from "r3f-perf";
 import { useFrame } from "@react-three/fiber";
 import {
-	AccumulativeShadows,
+	// AccumulativeShadows,
+	BakeShadows,
+	ContactShadows,
 	OrbitControls,
-	RandomizedLight,
+	Sky,
+	// RandomizedLight,
 	// SoftShadows,
-	// useHelper,
+	useHelper,
 } from "@react-three/drei";
-// import { DirectionalLightHelper } from "three";
+import { DirectionalLightHelper } from "three";
+import { useControls } from "leva";
 
 const App = () => {
 	const cube = useRef<THREE.Mesh>(null!);
 	const directionalLight = useRef<THREE.DirectionalLight>(null!);
 
-	// useHelper(directionalLight, DirectionalLightHelper, 1, "hotpink");
+	useHelper(directionalLight, DirectionalLightHelper, 1, "hotpink");
 
 	useFrame((state, delta) => {
 		// const time = state.clock.elapsedTime;
@@ -22,9 +26,34 @@ const App = () => {
 		cube.current.rotation.y += delta * 0.2;
 	});
 
+	const { color, opacity, blur } = useControls("Contact Shadowes", {
+		color: "#316d39",
+		opacity: {
+			value: 0.5,
+			min: 0,
+			max: 1,
+			step: 0.01,
+			label: "Opacity",
+		},
+		blur: {
+			value: 2.8,
+			min: 0,
+			max: 10,
+			step: 1,
+			label: "Blur",
+		},
+	});
+
+	const { sunPosition } = useControls("Sun", {
+		sunPosition: {
+			value: [1, 2, 3],
+			label: "Position",
+		},
+	});
+
 	return (
 		<>
-			{/* <BakeShadows /> */}
+			<BakeShadows />
 			{/* <SoftShadows
 				frustum={3.75}
 				size={0.005}
@@ -40,7 +69,7 @@ const App = () => {
 			<OrbitControls makeDefault />
 
 			{/* works only on plane geometry */}
-			<AccumulativeShadows
+			{/* <AccumulativeShadows
 				position={[0, -0.99, 0]}
 				scale={10}
 				color="#316d39"
@@ -57,11 +86,22 @@ const App = () => {
 					intensity={1}
 					bias={0.001}
 				/>
-			</AccumulativeShadows>
+			</AccumulativeShadows> */}
+
+			{/* works only on plane geometry */}
+			<ContactShadows
+				position={[0, -0.99, 0]}
+				scale={10}
+				resolution={512}
+				far={5}
+				color={color}
+				opacity={opacity}
+				blur={blur}
+			/>
 
 			<directionalLight
 				ref={directionalLight}
-				position={[1, 2, 3]}
+				position={sunPosition}
 				intensity={1.5}
 				castShadow
 				shadow-mapSize={[1024, 1024]}
@@ -73,6 +113,8 @@ const App = () => {
 				shadow-camera-bottom={-5}
 			/>
 			<ambientLight intensity={0.5} />
+
+			<Sky sunPosition={sunPosition} />
 
 			<mesh position-x={-2} castShadow>
 				<sphereGeometry />
