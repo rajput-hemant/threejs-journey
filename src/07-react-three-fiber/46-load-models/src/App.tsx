3848d42a8844089159937f0a897b3f1ae6e3afa0
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2023-02-01 15:03:43 +0530

    added Fox model w/ animations, loading models w useGLTF hook
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- src/07-react-three-fiber/46-load-models/src/App.tsx
+++ src/07-react-three-fiber/46-load-models/src/App.tsx
@@ -5,6 +5,7 @@ import { OrbitControls } from "@react-three/drei";
 import Cube from "./models/Cube";
 import Hamburger from "./models/Hamburger";
 import FlightHelmet from "./models/FlightHelmet";
+import Fox from "./models/Fox";
 
 const App = () => {
 	return (
@@ -13,7 +14,13 @@ const App = () => {
 
 			<OrbitControls makeDefault />
 
-			<directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+			<directionalLight
+				castShadow
+				position={[1, 2, 3]}
+				intensity={1.5}
+				// to remove Shadow Acne i.e. object(s) casting shadow on itself
+				shadow-normalBias={0.04}
+			/>
 			<ambientLight intensity={0.5} />
 
 			<mesh
@@ -27,8 +34,9 @@ const App = () => {
 			</mesh>
 
 			<Suspense fallback={<Cube position-y={0.5} scale={[2, 3, 2]} />}>
-				<Hamburger />
+				<Hamburger scale={0.3} position-x={-2.5} position-y={-1} />
 				<FlightHelmet />
+				<Fox />
 			</Suspense>
 		</>
 	);
