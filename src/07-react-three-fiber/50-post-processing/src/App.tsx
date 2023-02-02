1385231a8b5af3e74e4d6738cb553ca4a83ee6c8
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2023-02-02 21:10:30 +0530

    added effects like Vignette, Glitch, Bloom, Noise, DepthofField, etc
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- src/07-react-three-fiber/50-post-processing/src/App.tsx
+++ src/07-react-three-fiber/50-post-processing/src/App.tsx
@@ -1,9 +1,46 @@
 import { Perf } from "r3f-perf";
 import { OrbitControls } from "@react-three/drei";
+import { BlendFunction, GlitchMode } from "postprocessing";
+import {
+	Bloom,
+	DepthOfField,
+	EffectComposer,
+	Glitch,
+	Noise,
+	Vignette,
+} from "@react-three/postprocessing";
 
 export default function Experience() {
 	return (
 		<>
+			<color args={["#000"]} attach="background" />
+
+			<EffectComposer
+			// multisampling={0}
+			>
+				<Vignette
+					offset={0.5}
+					darkness={0.9}
+					blendFunction={BlendFunction.NORMAL}
+				/>
+				<Glitch
+					// @ts-ignore
+					delay={[0.5, 1]}
+					// @ts-ignore
+					duration={[0.1, 0.3]}
+					// @ts-ignore
+					strength={[0.2, 0.4]}
+					mode={GlitchMode.SPORADIC}
+				/>
+				<Noise blendFunction={BlendFunction.SOFT_LIGHT} premultiply={false} />
+				<Bloom mipmapBlur intensity={0.5} luminanceThreshold={0} />
+				<DepthOfField
+					focusDistance={0.025}
+					focalLength={0.025}
+					bokehScale={6}
+				/>
+			</EffectComposer>
+
 			<Perf position="top-left" />
 
 			<OrbitControls makeDefault />
@@ -18,7 +55,13 @@ export default function Experience() {
 
 			<mesh castShadow position-x={2} scale={1.5}>
 				<boxGeometry />
-				<meshStandardMaterial color="mediumpurple" />
+				{/* <meshStandardMaterial
+					color="white"
+					emissive="orange"
+					emissiveIntensity={2}
+					toneMapped={false}
+				/> */}
+				<meshBasicMaterial color={[1.5, 1, 4]} toneMapped={false} />
 			</mesh>
 
 			<mesh
